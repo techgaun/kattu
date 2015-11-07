180412a2a15bd34d590bb7efabd5fd3d7e531a64
techgaun <coolsamar207@gmail.com>
2015-11-07 22:26:30 +0545

    add initial working but mock version

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var exec = require('child_process').exec;
+
+module.exports = function(cmd) {
+  exec(cmd, function (err, stdout, stderr) {
+    if (err !== null) {
+      if (stdout.length > 0) {
+        console.log(stdout.trim());
+      }
+      if (stderr.length > 0) {
+        console.log(stderr.trim());
+      }
+    }
+  });
+};
